2aa182e37889b98af58b3fa0c6a080b2499de0d2
Mauricio Molina <tica.molina57@gmail.com>
2025-03-15 18:12:40 -0300

    Llevo las etiquetas tr y td de table al componente Fila.jsx, le paso desde UserList como props la lista de usuarios (USUARIOS.js) y la recibo dentro de filas, mostrando los usuarios en la aplicación

--- src/components/Fila.jsx
+++ src/components/Fila.jsx
@@ -1,7 +1,18 @@
 
-const Fila = () => {
+const Fila = ({ usuario }) => {
   return (
-    <div>Fila</div>
+    <tr className="bg-white border-b border-gray-200">
+      <td className="px-6 py-4">{usuario.nombre}</td>
+      <td className="px-6 py-4">{usuario.apellido}</td>
+      <td className="px-6 py-4">{usuario.edad}</td>
+      <td className="px-6 py-4">{usuario.puesto}</td>
+      <td className="px-6 py-4">{usuario.foto}</td>
+      <td className="px-6 py-4">
+        <button className="px-4 py-2 bg-green-400 text-white rounded-lg hover:bg-green-700 cursor-pointer mr-2">Ver</button>
+        <button className="px-4 py-2 bg-yellow-400 text-white rounded-lg hover:bg-yellow-700 cursor-pointer mr-2">Editar</button>
+        <button className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 cursor-pointer mr-2">Borrar</button>
+      </td>
+    </tr>
   )
 }
 
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,3 +1,4 @@
+import USUARIOS from "../constants/USUARIOS"
 import Fila from "./Fila"
 
 const UserList = () => {
@@ -14,21 +15,15 @@ const UserList = () => {
         </tr>
       </thead>
       <tbody>
-        <tr className="bg-white border-b border-gray-200">
-          <td className="px-6 py-4">Maximiliano Luis</td>
-          <td className="px-6 py-4">Principe</td>
-          <td className="px-6 py-4">40</td>
-          <td className="px-6 py-4">Desarrollador Fullstack</td>
-          <td className="px-6 py-4">Foto</td>
-          <td className="px-6 py-4">
-            <button className="px-4 py-2 bg-green-400 text-white rounded-lg hover:bg-green-700 cursor-pointer mr-2">Ver</button>
-            <button className="px-4 py-2 bg-yellow-400 text-white rounded-lg hover:bg-yellow-700 cursor-pointer mr-2">Editar</button>
-            <button className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 cursor-pointer mr-2">Borrar</button>
-          </td>
-        </tr>
+
+        {
+          USUARIOS.map((usuario) => (
+            <Fila usuario={usuario} />
+
+          ))
+        }
       </tbody>
 
-    <Fila />
     </table>
   )
 }
